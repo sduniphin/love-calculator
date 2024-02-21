fef849117696fcaa6d55c6dc453bf982dde76ede
Sara Duniphin <sara.duniphin@gmail.com>
2024-02-21 06:19:44 -0600

    Add files via upload

--- love-calculator.js
+++ love-calculator.js
@@ -8,11 +8,11 @@ if (lovePercent === 100) {
     alert(name1 + " and " + name2 + " are a perfect match! 100%!");
 } else if (lovePercent > 80) {
     alert(name1 + " and " + name2 + " are a great match! " + lovePercent + "%.");
-} else if (lovePercent > 60) {
+} else if (lovePercent > 60 && lovePercent <= 80) {
     alert(name1 + " and " + name2 + " are a good match! " + lovePercent + "%.");
-} else if (lovePercent > 40) {
+} else if (lovePercent > 40 && lovePercent <= 60) {
     alert(name1 + " and " + name2 + " are a decent match! " + lovePercent + "%.");
-} else if (lovePercent > 20) {
+} else if (lovePercent > 20 && lovePercent <= 40) {
     alert(name1 + " and " + name2 + " are a poor match! " + lovePercent + "%.");
 }
 else {
